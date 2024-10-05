7e6f70b9270a197a9780389cba8de802d338daab
Hasan Ismail <hasanqqsp@gmail.com>
2024-10-05 18:33:50 +0700

    export conda

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib 
 
+plt = matplotlib.pyplot
 # Load dataset
 df = pd.read_csv('hour.csv')  
 
